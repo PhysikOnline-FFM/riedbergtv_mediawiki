eb81375ddbed99b2705a96954e8b834569b5a99a
root <root@physik-online-test2.th.physik.uni-frankfurt.de>
2016-11-09 15:49:09 +0100

    first commit

---
+++ extensions/EditAccount/EditAccount.alias.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Aliases for Special:EditAccount
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+/** Deutsch */
+$specialPageAliases['de'] = array(
+	'EditAccount' => array( 'BenuterBearbeiten', 'BenuterSchließen', 'BenuterDeaktivieren' ),
+);
+
+/** English */
+$specialPageAliases['en'] = array(
+	'EditAccount' => array( 'EditAccount', 'CloseAccount', 'DisableAccount' ),
+);
+
+/** Finnish (Suomi) */
+$specialPageAliases['fi'] = array(
+	'EditAccount' => array( 'Muokkaa käyttäjätunnusta', 'Sulje käyttäjätunnus', 'Sulje tunnus', 'Poista tunnus', 'Poista käyttäjätunnus' ),
+);
